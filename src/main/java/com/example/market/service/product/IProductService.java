d4b1bb07e16eac66f0a76300e631a66c0b778b41
quan200699 <51079189+quan200699@users.noreply.github.com>
2020-05-21 14:35:06 +0700

    add product repository and service

---
+++ src/main/java/com/example/market/service/product/IProductService.java
@@ -0,0 +1,7 @@
+package com.example.market.service.product;
+
+import com.example.market.model.Product;
+import com.example.market.service.IGeneralService;
+
+public interface IProductService extends IGeneralService<Product> {
+}

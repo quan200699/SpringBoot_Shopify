871209062d77302c9dbdca3389b48b74156f415f
quan200699 <51079189+quan200699@users.noreply.github.com>
2020-05-21 12:47:52 +0700

    add user and role entity

---
+++ src/main/java/com/example/market/model/RoleName.java
@@ -0,0 +1,6 @@
+package com.example.market.model;
+
+public enum RoleName {
+    ROLE_ADMIN,
+    ROLE_USER
+}
---
+++ src/main/java/com/example/market/model/User.java
@@ -0,0 +1,30 @@
+package com.example.market.model;
+
+import lombok.Data;
+
+import javax.persistence.*;
+import java.util.Set;
+
+@Entity
+@Data
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private String fullName;
+
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(name = "users_roles",
+            joinColumns = {@JoinColumn(name = "user_id")},
+            inverseJoinColumns = {@JoinColumn(name = "role_id")})
+    private Set<Role> roles;
+}

a315ce12585f0ed6adbb4dca918735eaa5bbd808
quan200699 <51079189+quan200699@users.noreply.github.com>
2020-05-21 14:05:47 +0700

    add security and jwt

---
+++ src/main/java/com/example/market/configuration/security/SecurityConfig.java
@@ -0,0 +1,115 @@
+package com.example.market.configuration.security;
+
+import com.example.market.configuration.custom.CustomAccessDeniedHandler;
+import com.example.market.configuration.custom.RestAuthenticationEntryPoint;
+import com.example.market.configuration.filter.JwtAuthenticationFilter;
+import com.example.market.model.Role;
+import com.example.market.model.RoleName;
+import com.example.market.model.User;
+import com.example.market.service.user.UserService;
+import com.example.market.service.role.RoleService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.BeanIds;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+import javax.annotation.PostConstruct;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    @Autowired
+    private UserService userService;
+
+    @Autowired
+    private RoleService roleService;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @Bean
+    public JwtAuthenticationFilter jwtAuthenticationFilter() {
+        return new JwtAuthenticationFilter();
+    }
+
+    @Bean(BeanIds.AUTHENTICATION_MANAGER)
+    @Override
+    public AuthenticationManager authenticationManager() throws Exception {
+        return super.authenticationManager();
+    }
+
+    @Bean
+    public RestAuthenticationEntryPoint restServicesEntryPoint() {
+        return new RestAuthenticationEntryPoint();
+    }
+
+    @Bean
+    public CustomAccessDeniedHandler customAccessDeniedHandler() {
+        return new CustomAccessDeniedHandler();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder(10);
+    }
+
+    @PostConstruct
+    public void init() {
+        List<User> users = (List<User>) userService.findAll();
+        List<Role> roleList = (List<Role>) roleService.findAll();
+        if (roleList.isEmpty()) {
+            Role roleAdmin = new Role();
+            roleAdmin.setId(1L);
+            roleAdmin.setName(RoleName.ROLE_ADMIN.toString());
+            roleService.save(roleAdmin);
+            Role roleCoach = new Role();
+            roleCoach.setId(2L);
+            roleCoach.setName(RoleName.ROLE_USER.toString());
+            roleService.save(roleCoach);
+        }
+        if (users.isEmpty()) {
+            User admin = new User();
+            Set<Role> roles = new HashSet<>();
+            roles.add(new Role(1L, RoleName.ROLE_ADMIN.toString()));
+            admin.setEmail("admin");
+            admin.setFullName("admin");
+            admin.setPassword(passwordEncoder.encode("123456"));
+            admin.setRoles(roles);
+            userService.save(admin);
+        }
+    }
+
+    @Autowired
+    public void configureGlobalSecurity(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userService).passwordEncoder(passwordEncoder());
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().ignoringAntMatchers("/**");
+        http.httpBasic().authenticationEntryPoint(restServicesEntryPoint());
+        http.authorizeRequests()
+                .antMatchers("/", "/login", "/**").permitAll()
+                .anyRequest().authenticated()
+                .and().csrf().disable()
+                .logout().logoutRequestMatcher(new AntPathRequestMatcher("/logout"));
+        http.addFilterBefore(jwtAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class)
+                .exceptionHandling().accessDeniedHandler(customAccessDeniedHandler());
+        http.sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+        http.cors();
+    }
+}

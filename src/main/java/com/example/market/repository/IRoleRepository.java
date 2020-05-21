fbda6e6573f1df85eb9dc743d20cafdcfdab558a
quan200699 <51079189+quan200699@users.noreply.github.com>
2020-05-21 13:28:21 +0700

    add role service and role repository

---
+++ src/main/java/com/example/market/repository/IRoleRepository.java
@@ -0,0 +1,9 @@
+package com.example.market.repository;
+
+import com.example.market.model.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface IRoleRepository extends JpaRepository<Role, Long> {
+}
---
+++ src/main/java/com/example/market/service/role/IRoleService.java
@@ -0,0 +1,7 @@
+package com.example.market.service.role;
+
+import com.example.market.model.Role;
+import com.example.market.service.IGeneralService;
+
+public interface IRoleService extends IGeneralService<Role> {
+}

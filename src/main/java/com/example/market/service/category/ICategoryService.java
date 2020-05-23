0c5710edcefef6b949437ff7d6fb5234481e0479
quan200699 <51079189+quan200699@users.noreply.github.com>
2020-05-23 21:47:52 +0700

    add category repo and service

---
+++ src/main/java/com/example/market/service/category/ICategoryService.java
@@ -0,0 +1,7 @@
+package com.example.market.service.category;
+
+import com.example.market.model.Category;
+import com.example.market.service.IGeneralService;
+
+public interface ICategoryService extends IGeneralService<Category> {
+}

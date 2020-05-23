7466d146b3489db78db1b91e91309617ec3dc47d
quan200699 <51079189+quan200699@users.noreply.github.com>
2020-05-23 21:45:49 +0700

    add category

---
+++ src/main/java/com/example/market/model/Category.java
@@ -0,0 +1,16 @@
+package com.example.market.model;
+
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Entity
+@Data
+public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+}

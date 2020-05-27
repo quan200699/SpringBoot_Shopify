8b1907bd4b33e9c1651f32869f6654fcfd636b4c
VuKieuAnh <vukieuanh.hnue@gmail.com>
2020-05-27 16:12:11 +0700

    crud warehousingbill

--- src/main/java/com/example/market/controller/WarehousingBillController.java
+++ src/main/java/com/example/market/controller/WarehousingBillController.java
@@ -10,6 +10,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Date;
+import java.util.Optional;
 
 @RestController
 @CrossOrigin("*")
@@ -27,4 +28,36 @@ public class WarehousingBillController {
         return new ResponseEntity<>(warehousingBillService.save(warehousingBill), HttpStatus.OK) ;
     }
 
+    @GetMapping
+    public ResponseEntity<Iterable<WarehousingBill>> getAllWarehousingBill(){
+        return new ResponseEntity<>(warehousingBillService.findAll(), HttpStatus.OK);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<WarehousingBill> editWarehousingBill(@PathVariable Long id, @RequestBody WarehousingBill warehousingBill){
+        Optional<WarehousingBill> warehousingBillOptional = warehousingBillService.findById(id);
+        if (!warehousingBillOptional.isPresent()){
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        warehousingBill.setId(id);
+        return new ResponseEntity<>(warehousingBillService.save(warehousingBill), HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<WarehousingBill> deleteWarehousingBill(@PathVariable Long id){
+        Optional<WarehousingBill> warehousingBillOptional = warehousingBillService.findById(id);
+        if (!warehousingBillOptional.isPresent()){
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        warehousingBillService.remove(id);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+    @GetMapping("/{id}")
+    public ResponseEntity<WarehousingBill> getWarehousingBill(@PathVariable Long id){
+        Optional<WarehousingBill> warehousingBillOptional = warehousingBillService.findById(id);
+        if (!warehousingBillOptional.isPresent()){
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        return new ResponseEntity<>(warehousingBillOptional.get(), HttpStatus.OK);
+    }
 }

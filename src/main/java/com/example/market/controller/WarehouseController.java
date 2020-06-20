78bfbb74a8111fa36d6ea4853cd31cb944ff5362
quan200699 <51079189+quan200699@users.noreply.github.com>
2020-06-20 21:40:48 +0700

    change api

--- src/main/java/com/example/market/controller/WarehouseController.java
+++ src/main/java/com/example/market/controller/WarehouseController.java
@@ -11,7 +11,7 @@ import java.util.Optional;
 
 @RestController
 @CrossOrigin("*")
-@RequestMapping("/warehouse")
+@RequestMapping("/warehouses")
 public class WarehouseController {
 
     @Autowired

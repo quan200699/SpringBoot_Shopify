88761f1d1275666d41c381cb0ce696edf180d0f7
quan200699 <51079189+quan200699@users.noreply.github.com>
2020-06-26 12:40:14 +0700

    add item service

---
+++ src/main/java/com/example/market/service/item/IItemService.java
@@ -0,0 +1,7 @@
+package com.example.market.service.item;
+
+import com.example.market.model.Item;
+import com.example.market.service.IGeneralService;
+
+public interface IItemService extends IGeneralService<Item> {
+}
---
+++ src/main/java/com/example/market/service/item/ItemService.java
@@ -0,0 +1,34 @@
+package com.example.market.service.item;
+
+import com.example.market.model.Item;
+import com.example.market.repository.IItemRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class ItemService implements IItemService {
+    @Autowired
+    private IItemRepository itemRepository;
+
+    @Override
+    public Iterable<Item> findAll() {
+        return itemRepository.findAll();
+    }
+
+    @Override
+    public Optional<Item> findById(Long id) {
+        return itemRepository.findById(id);
+    }
+
+    @Override
+    public Item save(Item item) {
+        return itemRepository.save(item);
+    }
+
+    @Override
+    public void remove(Long id) {
+        itemRepository.deleteById(id);
+    }
+}

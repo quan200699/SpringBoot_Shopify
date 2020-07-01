8f1398ac70ed07cc2256883a449c817e7dc47c3b
quan200699 <51079189+quan200699@users.noreply.github.com>
2020-07-01 13:38:01 +0700

    add order detail entity

--- src/main/java/com/example/market/model/OrdersDetail.java
+++ src/main/java/com/example/market/model/OrdersDetail.java
@@ -1,23 +1,18 @@
 package com.example.market.model;
 
-import com.example.market.model.auth.User;
 import lombok.Data;
 
 import javax.persistence.*;
-import java.util.Date;
 
 @Entity
 @Data
-public class Orders {
+public class OrdersDetail {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(nullable = false)
-    private Date createDate;
-
-    private boolean status;
+    private int amount;
 
     @ManyToOne
-    private User user;
+    private Product product;
 }

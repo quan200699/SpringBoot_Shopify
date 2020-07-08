7d5a665236535dc834b2c54d7cbeb5eb4ad79e30
quan200699 <51079189+quan200699@users.noreply.github.com>
2020-07-08 14:58:57 +0700

    add notification repo and service

---
+++ src/main/java/com/example/market/service/notification/INotificationService.java
@@ -0,0 +1,7 @@
+package com.example.market.service.notification;
+
+import com.example.market.model.Notification;
+import com.example.market.service.IGeneralService;
+
+public interface INotificationService extends IGeneralService<Notification> {
+}
---
+++ src/main/java/com/example/market/service/notification/NotificationService.java
@@ -0,0 +1,34 @@
+package com.example.market.service.notification;
+
+import com.example.market.model.Notification;
+import com.example.market.repository.INotificationRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class NotificationService implements INotificationService {
+    @Autowired
+    private INotificationRepository notificationRepository;
+
+    @Override
+    public Iterable<Notification> findAll() {
+        return notificationRepository.findAll();
+    }
+
+    @Override
+    public Optional<Notification> findById(Long id) {
+        return notificationRepository.findById(id);
+    }
+
+    @Override
+    public Notification save(Notification notification) {
+        return notificationRepository.save(notification);
+    }
+
+    @Override
+    public void remove(Long id) {
+        notificationRepository.deleteById(id);
+    }
+}

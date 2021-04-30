2c4fababb526decfb0edc47cd589d2c938c2facc
quan200699 <51079189+quan200699@users.noreply.github.com>
2021-04-30 15:06:36 +0700

    add bill and bill detail service

---
+++ src/main/java/com/example/market/service/billDetail/BillDetailService.java
@@ -0,0 +1,34 @@
+package com.example.market.service.billDetail;
+
+import com.example.market.model.BillDetail;
+import com.example.market.repository.IBillDetailRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class BillDetailService implements IBillDetailService {
+    @Autowired
+    private IBillDetailRepository billDetailRepository;
+
+    @Override
+    public Iterable<BillDetail> findAll() {
+        return billDetailRepository.findAll();
+    }
+
+    @Override
+    public Optional<BillDetail> findById(Long id) {
+        return billDetailRepository.findById(id);
+    }
+
+    @Override
+    public BillDetail save(BillDetail billDetail) {
+        return billDetailRepository.save(billDetail);
+    }
+
+    @Override
+    public void remove(Long id) {
+        billDetailRepository.deleteById(id);
+    }
+}
---
+++ src/main/java/com/example/market/service/billDetail/IBillDetailService.java
@@ -0,0 +1,7 @@
+package com.example.market.service.billDetail;
+
+import com.example.market.model.BillDetail;
+import com.example.market.service.IGeneralService;
+
+public interface IBillDetailService extends IGeneralService<BillDetail> {
+}
